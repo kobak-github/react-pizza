59136740a9793614771cf4abae521a313cfb980d
kobak-github <=konurbaevbm@gmail.com>
2022-12-05 03:36:21 +0600

    from the category menu render and the pizza block

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,20 @@
+import React from "react"
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, i) => {
+          return(
+            <li className={activeIndex === i ? "active" : ""} onClick={() => setActiveIndex(i)}>
+              {value}
+             </li>
+          )
+        })}
       </ul>
     </div>
   )
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,26 +1,32 @@
-function PizzaBlock() {
+import React, {useState} from 'react'
+
+function PizzaBlock({imageUrl, title, price, sizes, types}) {
+  const [activeType, setActiveType] = React.useState(-1)
+  const typeName = ['тонкое', 'традиционное']
+  
+  const [activeSize, setActiveSize] = React.useState(-1)
+
   return(
-    <div class="pizza-block">
+    <div className="pizza-block">
               <img
-                class="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                className="pizza-block__image"
+                src={imageUrl}
                 alt="Pizza"
               />
-              <h4 class="pizza-block__title">Чизбургер-пицца</h4>
-              <div class="pizza-block__selector">
+              <h4 className="pizza-block__title">{title}</h4>
+              <div className="pizza-block__selector">
                 <ul>
-                  <li class="active">тонкое</li>
-                  <li>традиционное</li>
+                  {types.map((typeId) => (<li className={activeType === typeId ? 'active' : ''} onClick={() => setActiveType(typeId)}>{typeName[typeId]}</li>))}
                 </ul>
                 <ul>
-                  <li class="active">26 см.</li>
-                  <li>30 см.</li>
-                  <li>40 см.</li>
+                  {sizes.map((size, i) => (
+                    <li className={activeSize === i ? 'active' : ''} onClick={() => setActiveSize(i)}>{size} см.</li>
+                  ))}
                 </ul>
               </div>
-              <div class="pizza-block__bottom">
-                <div class="pizza-block__price">от 395 ₽</div>
-                <div class="button button--outline button--add">
+              <div className="pizza-block__bottom">
+                <div className="pizza-block__price">от {price} ₽</div>
+                <button className="button button--outline button--add">
                   <svg
                     width="12"
                     height="12"
@@ -34,8 +40,8 @@ function PizzaBlock() {
                     />
                   </svg>
                   <span>Добавить</span>
-                  <i>2</i>
-                </div>
+                  <i>0</i>
+                </button>
               </div>
             </div>
   )
